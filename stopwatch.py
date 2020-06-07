4a12f5b163ca3e2cdfc6d4492b9f9f11be0443c7
anurag <anuragkumar07102003@gmail.com>
2020-06-07 20:09:19 +0530

    Added a non functioning Capture button. Added the time transition feature as the stopwatch will start in form [(min):(sec).(milli)] and as the stopwatch crosses 59 min it changes to [(hrs):(min):(sec)].

--- stopwatch.py
+++ stopwatch.py
@@ -48,10 +48,15 @@ class button:
 
         return False
 
-start_button = button((200, 200, 200), (0, 0, 0), 430, 635, 150, 55, ' Start ')
-start_button.font_size = 35
+
+capture_button = button((200, 200, 200), (0, 0, 0), 30, 635, 150, 55, 'Capture ')
+capture_button.font_size = 35
+
 reset_button = button((200, 200, 200), (0, 0, 0), 230, 635, 150, 55, ' Reset ')
 reset_button.font_size = 35
+
+start_button = button((200, 200, 200), (0, 0, 0), 430, 635, 150, 55, ' Start ')
+start_button.font_size = 35
 
 clock_button = button(bg, (200, 200, 200), 5, 0, 70, 30, ' Clock ')
 alarm_button = button(bg, (200, 200, 200), 93, 0, 70, 30, ' Alarm ')
@@ -60,39 +65,41 @@ timer_button = button(bg, (200, 200, 200), 335, 0, 70, 30, ' Timer ')
 
 class stopwatch:
     def __init__(self, x, y):
+        self.x, self.y = x, y
         self.font0 = pygame.font.Font(ubuntu_font, 75)
         self.font1 = pygame.font.Font(ubuntu_font, 85)
 
         self.hrs_text = self.font0.render('', True, line, bg)
         self.hrs_textRect = self.hrs_text.get_rect()
-        self.hrs_textRect.center = x + self.hrs_text.get_width() // 2, y + self.hrs_text.get_height() // 2
 
         self.min_text = self.font0.render('', True, line, bg)
         self.min_textRect = self.min_text.get_rect()
-        self.min_textRect.center = 150 + x + self.min_text.get_width() // 2, y + self.min_text.get_height() // 2
 
         self.sec_text = self.font0.render('', True, line, bg)
         self.sec_textRect = self.sec_text.get_rect()
-        self.sec_textRect.center = 300 + x + self.sec_text.get_width(), y + self.sec_text.get_height() // 2
+
+        self.milli_text = self.font0.render('', True, line, bg)
+        self.milli_textRect = self.milli_text.get_rect()
 
         self.colan_text = self.font1.render(':', True, line, bg)
         self.colan_textRect = self.colan_text.get_rect()
-        self.colan_textRect.center = 105 + x + self.colan_text.get_width() // 2, y - 15 + self.colan_text.get_height() // 2
+        self.colan_textRect.center = 105 + self.x + self.colan_text.get_width() // 2, self.y - 15 + self.colan_text.get_height() // 2
 
         self.colan_dot = self.font1.render('.', True, line, bg)
         self.colan_dotRect = self.colan_dot.get_rect()
-        self.colan_dotRect.center = 255 + x + self.colan_dot.get_width() // 2, y - 15 + self.colan_dot.get_height() // 2
+        self.colan_dotRect.center = 255 + self.x + self.colan_dot.get_width() // 2, self.y - 15 + self.colan_dot.get_height() // 2
 
 stopwatch_obj = stopwatch(100, 50)
 
 def stopwatch_window():
     pygame.display.set_caption('Stop Watch')
     run = True
-    duration, sec, min, millis = 0, 0, 0, 0
+    duration, millis, sec, min, hrs = 0, 0, 0, 0, 0
     stopwatch_run = False
     active_status = False
+    state = False
     while run:
-        duration += 1
+        duration += 100
         time.sleep(0.01)
         window.fill(bg)
         stopwatch_button.text_color = (151, 147, 245)
@@ -115,20 +122,52 @@ def stopwatch_window():
             sec += 1 if duration % 100 == 0 else 0
             sec %= 60
             min += 1 if duration % 6000 == 0 else 0
+            if min == 60:
+                state = True
+            min %= 60
+            hrs += 1 if duration % 360000 == 0 else 0
             start_button.text = ' Pause '
         elif not active_status:
             start_button.text = ' Start '
         else:
             start_button.text = ' Resume '
 
-        stopwatch_obj.hrs_text = stopwatch_obj.font0.render('%02d' % min, True, line, bg)
-        window.blit(stopwatch_obj.hrs_text, stopwatch_obj.hrs_textRect)
+        if not state:
+            minx = stopwatch_obj.x - 8
+            secx = stopwatch_obj.x + 142
+            millix = stopwatch_obj.x + 292
+            stopwatch_obj.min_textRect.center = minx, stopwatch_obj.y + stopwatch_obj.min_text.get_height() // 2
+            stopwatch_obj.sec_textRect.center = secx, stopwatch_obj.y + stopwatch_obj.sec_text.get_height() // 2
+            stopwatch_obj.milli_textRect.center = millix, stopwatch_obj.y + stopwatch_obj.milli_text.get_height() // 2
 
-        stopwatch_obj.min_text = stopwatch_obj.font0.render('%02d' % sec, True, line, bg)
-        window.blit(stopwatch_obj.min_text, stopwatch_obj.min_textRect)
+            stopwatch_obj.min_text = stopwatch_obj.font0.render('%02d' % min, True, line, bg)
+            window.blit(stopwatch_obj.min_text, stopwatch_obj.min_textRect)
 
-        stopwatch_obj.sec_text = stopwatch_obj.font0.render('%02d' % millis, True, line, bg)
-        window.blit(stopwatch_obj.sec_text, stopwatch_obj.sec_textRect)
+            stopwatch_obj.sec_text = stopwatch_obj.font0.render('%02d' % sec, True, line, bg)
+            window.blit(stopwatch_obj.sec_text, stopwatch_obj.sec_textRect)
+
+            stopwatch_obj.milli_text = stopwatch_obj.font0.render('%02d' % millis, True, line, bg)
+            window.blit(stopwatch_obj.milli_text, stopwatch_obj.milli_textRect)
+
+            stopwatch_obj.colan_dot = stopwatch_obj.font1.render('.', True, line, bg)
+        else:
+            hrsx = stopwatch_obj.x - 8
+            minx = stopwatch_obj.x + 142
+            secx = stopwatch_obj.x + 292
+            stopwatch_obj.hrs_textRect.center = hrsx, stopwatch_obj.y + stopwatch_obj.hrs_text.get_height() // 2
+            stopwatch_obj.min_textRect.center = minx, stopwatch_obj.y + stopwatch_obj.min_text.get_height() // 2
+            stopwatch_obj.sec_textRect.center = secx, stopwatch_obj.y + stopwatch_obj.sec_text.get_height() // 2
+
+            stopwatch_obj.hrs_text = stopwatch_obj.font0.render('%02d' % hrs, True, line, bg)
+            window.blit(stopwatch_obj.hrs_text, stopwatch_obj.hrs_textRect)
+
+            stopwatch_obj.min_text = stopwatch_obj.font0.render('%02d' % min, True, line, bg)
+            window.blit(stopwatch_obj.min_text, stopwatch_obj.min_textRect)
+
+            stopwatch_obj.sec_text = stopwatch_obj.font0.render('%02d' % sec, True, line, bg)
+            window.blit(stopwatch_obj.sec_text, stopwatch_obj.sec_textRect)
+
+            stopwatch_obj.colan_dot = stopwatch_obj.font1.render(':', True, line, bg)
 
         window.blit(stopwatch_obj.colan_text, stopwatch_obj.colan_textRect)
 
@@ -136,6 +175,7 @@ def stopwatch_window():
 
         start_button.draw(window, bg)
         reset_button.draw(window, bg)
+        capture_button.draw(window, bg)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -150,9 +190,14 @@ def stopwatch_window():
                     active_status = True
 
                 if reset_button.isOver(pos):
-                    duration, sec, min, millis = 0, 0, 0, 0
+                    duration, sec, min, millis, hrs = 0, 0, 0, 0, 0
                     stopwatch_run = False
                     active_status = False
+                    state = False
+
+                if capture_button.isOver(pos):
+                    print("lap")
+
                 # if clock_button.isOver(pos):
                 #     clock_window()
                 # if alarm_button.isOver(pos):
@@ -185,6 +230,11 @@ def stopwatch_window():
                     reset_button.color = (151, 147, 245)
                 else:
                     reset_button.color = (200, 200, 200)
+
+                if capture_button.isOver(pos):
+                    capture_button.color = (151, 147, 245)
+                else:
+                    capture_button.color = (200, 200, 200)
 
         pygame.display.update()
 
