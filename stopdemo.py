b8b986a300d538d71fdb6d7a9f98be0762c77093
anurag <anuragkumar07102003@gmail.com>
2020-06-09 12:12:39 +0530

    Almost done, Improved the audo relocation of the capture list, relocated the spl_lap button, the capture list and resized the main stopwatch timer, its working fucking well.

--- stopdemo.py
+++ stopdemo.py
@@ -59,14 +59,15 @@ reset_button.font_size = 35
 start_button = button((200, 200, 200), (0, 0, 0), 430, 635, 150, 55, ' Start ')
 start_button.font_size = 35
 
-state_button = button(bg, (200, 200, 200), 335, 0, 70, 30, ' ')
+state_button = button(bg, (200, 200, 200), 50, 595, 110, 35, ' ')
+state_button.font_size = 30
 
 
 class stopwatch:
     def __init__(self, x, y):
         self.x, self.y = x, y
-        self.font0 = pygame.font.Font(ubuntu_font, 75)
-        self.font1 = pygame.font.Font(ubuntu_font, 85)
+        self.font0 = pygame.font.Font(ubuntu_font, 95)
+        self.font1 = pygame.font.Font(ubuntu_font, 105)
 
         self.hrs_text = self.font0.render('', True, line, bg)
         self.hrs_textRect = self.hrs_text.get_rect()
@@ -82,14 +83,14 @@ class stopwatch:
 
         self.colan_text = self.font1.render(':', True, line, bg)
         self.colan_textRect = self.colan_text.get_rect()
-        self.colan_textRect.center = 105 + self.x + self.colan_text.get_width() // 2, self.y - 15 + self.colan_text.get_height() // 2
+        self.colan_textRect.center = 100 + self.x + self.colan_text.get_width() // 2, self.y - 15 + self.colan_text.get_height() // 2
 
         self.colan_dot = self.font1.render('.', True, line, bg)
         self.colan_dotRect = self.colan_dot.get_rect()
         self.colan_dotRect.center = 255 + self.x + self.colan_dot.get_width() // 2, self.y - 15 + self.colan_dot.get_height() // 2
 
 
-stopwatch_obj = stopwatch(100, 50)
+stopwatch_obj = stopwatch(100, 60)
 
 
 class capture:
@@ -148,55 +149,53 @@ class capture:
         self.milli_textRect.center = self.x + 240, y
 
 
-cap1 = capture(1, 200, 0, 0, 0, 0, 0)
-cap2 = capture(2, 200, 0, 0, 0, 0, 0)
-cap3 = capture(3, 200, 0, 0, 0, 0, 0)
-cap4 = capture(4, 200, 0, 0, 0, 0, 0)
-cap5 = capture(5, 200, 0, 0, 0, 0, 0)
-cap6 = capture(6, 200, 0, 0, 0, 0, 0)
-cap7 = capture(7, 200, 0, 0, 0, 0, 0)
-cap8 = capture(8, 200, 0, 0, 0, 0, 0)
-cap9 = capture(9, 200, 0, 0, 0, 0, 0)
-cap10 = capture(10, 200, 0, 0, 0, 0, 0)
-cap11 = capture(11, 200, 0, 0, 0, 0, 0)
-cap12 = capture(12, 200, 0, 0, 0, 0, 0)
-cap13 = capture(13, 200, 0, 0, 0, 0, 0)
-cap14 = capture(14, 200, 0, 0, 0, 0, 0)
-cap15 = capture(15, 200, 0, 0, 0, 0, 0)
-cap16 = capture(16, 200, 0, 0, 0, 0, 0)
-cap17 = capture(17, 200, 0, 0, 0, 0, 0)
-cap18 = capture(18, 200, 0, 0, 0, 0, 0)
-cap19 = capture(19, 200, 0, 0, 0, 0, 0)
-cap20 = capture(20, 200, 0, 0, 0, 0, 0)
-cap21 = capture(21, 200, 0, 0, 0, 0, 0)
-cap22 = capture(22, 200, 0, 0, 0, 0, 0)
-cap23 = capture(23, 200, 0, 0, 0, 0, 0)
-cap24 = capture(24, 200, 0, 0, 0, 0, 0)
-cap25 = capture(25, 200, 0, 0, 0, 0, 0)
-cap26 = capture(26, 200, 0, 0, 0, 0, 0)
-cap27 = capture(27, 200, 0, 0, 0, 0, 0)
-cap28 = capture(28, 200, 0, 0, 0, 0, 0)
-cap29 = capture(29, 200, 0, 0, 0, 0, 0)
-cap30 = capture(30, 200, 0, 0, 0, 0, 0)
+cap1 = capture(1,   210, 0, 0, 0, 0, 0)
+cap2 = capture(2,   210, 0, 0, 0, 0, 0)
+cap3 = capture(3,   210, 0, 0, 0, 0, 0)
+cap4 = capture(4,   210, 0, 0, 0, 0, 0)
+cap5 = capture(5,   210, 0, 0, 0, 0, 0)
+cap6 = capture(6,   210, 0, 0, 0, 0, 0)
+cap7 = capture(7,   210, 0, 0, 0, 0, 0)
+cap8 = capture(8,   210, 0, 0, 0, 0, 0)
+cap9 = capture(9,   210, 0, 0, 0, 0, 0)
+cap10 = capture(10, 210, 0, 0, 0, 0, 0)
+cap11 = capture(11, 210, 0, 0, 0, 0, 0)
+cap12 = capture(12, 210, 0, 0, 0, 0, 0)
+cap13 = capture(13, 210, 0, 0, 0, 0, 0)
+cap14 = capture(14, 210, 0, 0, 0, 0, 0)
+cap15 = capture(15, 210, 0, 0, 0, 0, 0)
+cap16 = capture(16, 210, 0, 0, 0, 0, 0)
+cap17 = capture(17, 210, 0, 0, 0, 0, 0)
+cap18 = capture(18, 210, 0, 0, 0, 0, 0)
+cap19 = capture(19, 210, 0, 0, 0, 0, 0)
+cap20 = capture(20, 210, 0, 0, 0, 0, 0)
+cap21 = capture(21, 210, 0, 0, 0, 0, 0)
+cap22 = capture(22, 210, 0, 0, 0, 0, 0)
+cap23 = capture(23, 210, 0, 0, 0, 0, 0)
+cap24 = capture(24, 210, 0, 0, 0, 0, 0)
+cap25 = capture(25, 210, 0, 0, 0, 0, 0)
+cap26 = capture(26, 210, 0, 0, 0, 0, 0)
+cap27 = capture(27, 210, 0, 0, 0, 0, 0)
+cap28 = capture(28, 210, 0, 0, 0, 0, 0)
+cap29 = capture(29, 210, 0, 0, 0, 0, 0)
+cap30 = capture(30, 210, 0, 0, 0, 0, 0)
 
 up = pygame.image.load('images/arrup.png')
 up.set_colorkey(white)
+up_cord = (270, 170)
 down = pygame.image.load('images/arrdw.png')
 down.set_colorkey(white)
-up_x = 250
-up_y = 140
-down_x = 250
-down_y = 550
+down_cord = (270, 570)
 
 
 def isOverStopwatch(pos):
-    if up_x < pos[0] < up_x + up.get_width():
-        if up_y < pos[1] < up_y + up.get_height():
-            return "add"
+    if up_cord[0] < pos[0] < up_cord[0] + up.get_width():
+        if up_cord[1] < pos[1] < up_cord[1] + up.get_height():
+            return "up"
 
-    if down_x < pos[0] < down_x + down.get_width():
-        if down_y < pos[1] < down_y + down.get_height():
-            return 'sub'
+    if down_cord[0] < pos[0] < down_cord[0] + down.get_width():
+        if down_cord[1] < pos[1] < down_cord[1] + down.get_height():
+            return 'down'
 
 
 s_run = True
@@ -207,7 +206,7 @@ s_state = False
 s_counter = 0
 s_caplsit = [cap1, cap2, cap3, cap4, cap5, cap6, cap7, cap8, cap9, cap10, cap11, cap12, cap13, cap14, cap15, cap16,
              cap17, cap18, cap19, cap20, cap21, cap22, cap23, cap24, cap25, cap26, cap27, cap28, cap29, cap30]
-s_position = [220, 270, 320, 370, 420, 470, 520]
+s_position = [240, 290, 340, 390, 440, 490, 540]
 s_spl_lap = True
 s_cap_count = 0
 
@@ -228,17 +227,20 @@ def stopwatch_window():
             s_cap_count += 1
             if s_cap_count > 7:
                 s_counter = s_cap_count-7
+            else:
+                s_counter = 0
             if not s_spl_lap:
                 s_duration, s_sec, s_min, s_milli, s_hrs = 0, 0, 0, 0, 0
 
 
     def s_reset():
-        global s_hrs, s_min, s_milli, s_sec, s_duration, s_stopwatch_run, s_active_status, s_state, s_cap_count
+        global s_hrs, s_min, s_milli, s_sec, s_duration, s_stopwatch_run, s_active_status, s_state, s_cap_count, s_counter
         s_duration, s_sec, s_min, s_milli, s_hrs = 0, 0, 0, 0, 0
         s_stopwatch_run = False
         s_active_status = False
         s_state = False
         s_cap_count = 0
+        s_counter = 0
         for i in s_caplsit:
             i.sn_text = i.font3.render("%02d)" % i.sn, True, line, bg)
             i.hrs_text = i.font0.render("%02d" % s_hrs, True, line, bg)
@@ -247,7 +249,7 @@ def stopwatch_window():
             i.milli_text = i.font0.render("%02d" % s_milli, True, line, bg)
 
     while s_run:
-        s_duration += 100
+        s_duration += 1
         time.sleep(0.01)
         pos_counter = 0
         window.fill(bg)
@@ -267,9 +269,9 @@ def stopwatch_window():
             start_button.text = ' Resume '
 
         if not s_state:
-            minx = stopwatch_obj.x - 8
-            secx = stopwatch_obj.x + 142
-            millix = stopwatch_obj.x + 292
+            minx = stopwatch_obj.x - 20
+            secx = stopwatch_obj.x + 140
+            millix = stopwatch_obj.x + 290
             stopwatch_obj.min_textRect.center = minx, stopwatch_obj.y + stopwatch_obj.min_text.get_height() // 2
             stopwatch_obj.sec_textRect.center = secx, stopwatch_obj.y + stopwatch_obj.sec_text.get_height() // 2
             stopwatch_obj.milli_textRect.center = millix, stopwatch_obj.y + stopwatch_obj.milli_text.get_height() // 2
@@ -285,9 +287,9 @@ def stopwatch_window():
 
             stopwatch_obj.colan_dot = stopwatch_obj.font1.render('.', True, line, bg)
         else:
-            hrsx = stopwatch_obj.x - 8
-            minx = stopwatch_obj.x + 142
-            secx = stopwatch_obj.x + 292
+            hrsx = stopwatch_obj.x - 20
+            minx = stopwatch_obj.x + 140
+            secx = stopwatch_obj.x + 290
             stopwatch_obj.hrs_textRect.center = hrsx, stopwatch_obj.y + stopwatch_obj.hrs_text.get_height() // 2
             stopwatch_obj.min_textRect.center = minx, stopwatch_obj.y + stopwatch_obj.min_text.get_height() // 2
             stopwatch_obj.sec_textRect.center = secx, stopwatch_obj.y + stopwatch_obj.sec_text.get_height() // 2
@@ -321,8 +323,8 @@ def stopwatch_window():
         reset_button.draw(window, bg)
         capture_button.draw(window, bg)
         state_button.draw(window, bg)
-        window.blit(up, (up_x, up_y))
-        window.blit(down, (down_x, down_y))
+        window.blit(up, up_cord)
+        window.blit(down, down_cord)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -342,12 +344,12 @@ def stopwatch_window():
                 if capture_button.isOver(pos):
                     capture()
 
-                if state_button.isOver(pos):
+                if state_button.isOver(pos) and not s_active_status:
                     s_spl_lap = not s_spl_lap
 
-                if isOverStopwatch(pos) == 'add' and s_counter > 0:
+                if isOverStopwatch(pos) == 'up' and s_counter > 0:
                     s_counter -= 1
-                if isOverStopwatch(pos) == 'sub' and s_counter < 23:
+                if isOverStopwatch(pos) == 'down' and s_counter < 23:
                     s_counter += 1
 
             if event.type == pygame.MOUSEMOTION:
